6b886d2fb19d63c0d5893963839c77ae1a964039
Darya Sakalova <Darya.Sakalova@thomsonreuters.com>
2017-02-27 16:02:07 +0300

    added src for semaphore classes and policy file.

---
+++ semaphore/v2/com/epam/mentoring/classloading/model/semaphore/Semaphore.java
@@ -0,0 +1,8 @@
+package com.epam.mentoring.classloading.model.semaphore;
+
+public class Semaphore {
+	
+	public void lever() {
+		System.out.println("It works for the second version of Semaphore!");
+	}
+}
